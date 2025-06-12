bb33abfe7e1b1f40b3cff8d27e674ce52f22e31d
Wei Lu <way138730101@gmail.com>
2025-06-12 15:24:50 -0400

    added scene manager

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    // void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if (other.tag == "Ground")
-    //     {
-    //         Debug.Log("Ouch, hit my head!");
-    //     }
-    // }
-
-    void OnCollisionEnter2D(Collision2D other) {
+    [SerializeField] float loadDelay = 0.5f;
+    void OnCollisionEnter2D(Collision2D other)
+    {
         if (other.gameObject.tag == "Ground")
-        { 
-            Debug.Log("Ouch, hit my head!");
+        {
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
